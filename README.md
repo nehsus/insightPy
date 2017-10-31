7739ce521d5f1b0a0eeecdfab2525ced4fafc406
Sushen Kumar <sushenksharma@gmail.com>
2017-10-31 12:56:57 +0530

    _readme fix

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # insightPy
+bd.py>starts webserver at localost:5000
+templates> dynamic html dir
+static> css,js files
+mongod utils:
+    ./mongo db.pene2.test.find()                    -- lists all elements in the collection 'test' of db 'pene2'
+            db.pene2.insertOne({"name": "Succubus") -- inserts Succubus to db
+            db.pene2.delete_many({})                --  remove all elements from collection
+    ***     
+    ./mongod --dbpath ~/insightPy/db --smallfiles   -- set path to db
+
+    
--- bd.py
+++ bd.py
@@ -1,24 +1,53 @@
-from flask import Flask, render_template, request
+# 2017 github:/nehsus
+#test with Suicides.csv in "input"
+from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 from pymongo import MongoClient
 from flask import jsonify
 import json, ast
+import pandas as pandu
+import os
+from werkzeug import secure_filename
+
 
 app = Flask(__name__)
 client = MongoClient("localhost", 27017)
 
-@app.route("/")
+def kindlyDB(haha): #Parse uploaded CSV using pandas and pymongo to db
+    dbNOW= client['pene']#select current db name
+    collNOW= 'things' #current collection name
+    currentDB=dbNOW[collNOW]
+    finalJO= pandu.read_csv(haha)#parse csv'
+    
+    finalJOson= json.loads(finalJO.to_json(orient= 'records'))
+    currentDB.delete_many({})
+    currentDB.insert(finalJOson)
+
+
+
+@app.route('/')
+@app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route("/d")
-def d():
-    return request.body.args("name")
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+    response='notOK'
+    if request.method == 'POST':
+        file=request.files['file']
+        if file:
+            response= 'OK'
+            filename= secure_filename(file.filename)
+            file.save(filename)
+            return response
+            kindlyDB(filename)
+        else:
+            return redirect(url_for('index'))
 
 @app.route('/data')
 def data():
     st1=[]
-    dat= client["pene2"].test.find()
+    dat= client["pene"].things.find()
     for i in dat:
         #passes json from pene database into js removes bloat
         del i["_id"]
@@ -29,7 +58,7 @@ def data():
 #rtype {"name": "BT"}
 
 if __name__ == "__main__":
-    print("\n\tConnected to Nehsuspace:27017\n\n")
-    app.run()
+    print("\n Established at :27017 \n")
+    app.run(debug=True)
 
 
